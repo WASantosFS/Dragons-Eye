732257d9bb6baaed7c13972c2ad65d54aac3215b
William A. Santos <baekgu@live.com>
2020-10-21 17:33:02 -0400

    Updated formatting replacements and ReadMe.

--- DragonsEye/DragonsEye/Formatting.cs
+++ DragonsEye/DragonsEye/Formatting.cs
@@ -7,12 +7,17 @@ namespace DragonsEye
 {
     public class Formatting
     {
-        //{" ", "!", "?", "'", ":", ";", "(", ")", "-", ",", "."};
-        //{"YX", "XD", "YD", "ZD", "XX", "XY", "KKA", "KKB", "YY", "ZZ", "XZ"};
+        /// <summary>
+        /// Formatting function that replaces punctuation and vice-versa. 
+        /// DOES NOT include ", _, /, \, =, +, {, }, [, ], &, *, ^, %, $, #, @, ~, `, <, and >.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="isEnciphered"></param>
+        /// <returns></returns>
         public string Format(string message, bool isEnciphered)
         {
-            List<string> symbols = new List<string>() { " ", "!", "?", "'", ":", ";", "(", ")", "-", ",", "." };
-            List<string> substitutes = new List<string>() { "YX", "XD", "YD", "ZD", "XX", "XY", "KKA", "KKB", "YY", "ZZ", "XZ" };
+            List<string> symbols = new List<string>() { " ", "!", "?", "'", ":", "(", ")", "-", ",", "." };
+            List<string> substitutes = new List<string>() { "QQ", "XD", "JQ", "ZD", "XX", "KKA", "KKB", "XY", "ZZ", "XZ" };
 
             for (int i = 0; i < symbols.Count; i++)
             {
@@ -29,6 +34,13 @@ namespace DragonsEye
             return message;
         }
 
+        /// <summary>
+        /// Grouping method so that the results are printed out in groups of 4. 
+        /// Having major issues with trying to also separate it into lines. Each \n is treated as an "enter" key stroke in console.
+        /// Console.WriteLine(), Environment.NewLine with {0} notation, and \r\n all have the same issue.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public string Grouping(string message)
         {
             int count = 5;
@@ -48,6 +60,11 @@ namespace DragonsEye
             return message;
         }
 
+        /// <summary>
+        /// Removes the spaces that were inserted during Grouping. 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public string Degrouping(string message)
         {
             return message.Replace(" ", "");
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-Dragons-Eye
+Project Name: Dragon's Eye
+
+Purpose: To develop an Enigma machine-inspired enciphering program.
+
+Description: This project started after the initial development of Steel Iris (an Enigma Machine emulator) ran into road blocks that, due to unmaintanable code, had to be placed on hold. Steel Iris did not follow the TDD paradigm. Following TDD, Dragon's Eye has yielded a much more managable program. The development is much more strict in that each iterative step must yield a working program in that it can encipher and decipher.
+
+TO-DO:
+- [x] Shift method
+- [x] Cipher method (1 ring)
+- [x] Implement reflector
+- [x] Implement reverse pass through
+- [x] Initial character setting
+- [x] Shift with each letter en/deciphered
+- [x] Format method (Punctuation)
+- [x] Group method (Groups of 4)
+- [ ] Implement offsets
+- [ ] Implement plugboard
+- [ ] Implement 2 rings
+- [ ] Implement 3 rings
+- [ ] Implement 4 rings
+
+BUGS:
+- [ ] Cannot encipher or decipher back-to-back
+- [x] Punctuation replacements can cause issues "way." -> "wa z"
